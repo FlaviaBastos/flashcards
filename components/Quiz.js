2c8674611fbda2a0a838386f106ca66faf749888
FlaviaBastos <flasanches@gmail.com>
2018-02-19 11:11:00 -0400

    refactor: cleanup styles

--- components/Quiz.js
+++ components/Quiz.js
@@ -70,12 +70,12 @@ export default class Quiz extends Component {
               />
         )}
         <TouchableOpacity
-          style={styles.submitBtnCorr}
+          style={[styles.submitBtn, {backgroundColor: '#008B00'}]}
           onPress={this.correct}>
             <Text style={styles.submitBtnText}>CORRECT</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          style={styles.submitBtnInc}
+          style={[styles.submitBtn, {backgroundColor: '#FF0000'}]}
           onPress={this.incorrect}>
             <Text style={styles.submitBtnText}>INCORRECT</Text>
         </TouchableOpacity>
@@ -85,45 +85,18 @@ export default class Quiz extends Component {
   }
 }
 
-// see how to apply style and and color separatelly
-// syntax: style={[styles.row, {justifyContent: 'space-between'}]}
-// make the all buttons the same length
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  btnFlip: {
-    backgroundColor: '#fff',
+  submitBtn: {
     margin: 10,
     padding: 10,
     paddingLeft: 30,
     paddingRight: 30,
     height: 45,
-    borderRadius: 2,
-    alignSelf: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  submitBtnCorr: {
-    backgroundColor: '#008B00',
-    margin: 10,
-    padding: 10,
-    paddingLeft: 30,
-    paddingRight: 30,
-    height: 45,
-    borderRadius: 2,
-    alignSelf: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  submitBtnInc: {
-    backgroundColor: '#FF0000',
-    margin: 10,
-    padding: 10,
-    paddingLeft: 30,
-    paddingRight: 30,
-    height: 45,
-    borderRadius: 2,
+    width: 190,
+    borderRadius: 6,
     alignSelf: 'center',
     justifyContent: 'center',
     alignItems: 'center',
@@ -133,25 +106,11 @@ const styles = StyleSheet.create({
     fontSize: 22,
     textAlign: 'center',
   },
-  flipCardText: {
-    color: '#FF9912',
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
   center: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     marginLeft: 30,
     marginRight: 30,
-  },
-  input: {
-    width: 200,
-    height: 44,
-    padding: 8,
-    borderWidth: 1,
-    borderColor: '#757575',
-    margin: 50,
   },
 })
