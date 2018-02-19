f0410aaf2a6c5dbd426d64fbd11e7f3c39de044f
FlaviaBastos <flasanches@gmail.com>
2018-02-19 14:07:10 -0400

    feat: add tabNavigation [WIP]

--- App.js
+++ App.js
@@ -6,6 +6,40 @@ import AddQuestion from './components/AddQuestion'
 import DeckDetail from './components/DeckDetail'
 import DeckList from './components/DeckList'
 import Quiz from './components/Quiz'
+import { TabNavigator } from 'react-navigation'
+
+const Tabs = TabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'DECKS',
+    }
+  },
+  AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'NEW DECK',
+    }
+  }
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: '#FF9912',
+    style: {
+      height: 56,
+      backgroundColor: '#fff',
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
 
 export default class App extends React.Component {
   render() {
@@ -15,7 +49,8 @@ export default class App extends React.Component {
         {/* <AddQuestion toDeck="A1" /> */}
         {/* <DeckDetail selectedDeck="A1" /> */}
         {/* <DeckList /> */}
-        <Quiz selectedDeck="A1" />
+        {/* <Quiz selectedDeck="A1" /> */}
+        <Tabs />
       </View>
     )
   }
@@ -24,8 +59,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   }
 })
